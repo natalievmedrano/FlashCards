8cc20470038c513fc3face0e7113ef0823720751
natalievmedrano <nataliemed0403@gmail.com>
2023-02-09 13:51:40 -0600

    changed background colors + added new card button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import SideBar from './Components/SideBar/Sidebar';
 //import Card from './Components/Card/Card';
 import CardContainer from './Components/CardContainer/CardContainer';
-
+import axios from 'axios';
 
 function App() {
   const[flashcards,setFlashCards] = useState(SampleApp)
+
+  useEffect(()=>{
+    
+  },[])
+
   return (
     <div>
      <Header/>
--- src/Components/SideBar/Sidebar.jsx
+++ src/Components/SideBar/Sidebar.jsx
@@ -7,6 +7,7 @@ const SideBar = ({}) => {
                 <tr><a href="http://127.0.0.1:8000/api/collections/1/">Flutter</a></tr>
                 <tr><a href="http://127.0.0.1:8000/api/collections/2/">CSS</a></tr>
                 <tr><a href="http://127.0.0.1:8000/api/collections/3/">PANDAS</a></tr>
+                <button> <a href="http://127.0.0.1:8000/api/collections/2/cards/"> ADD CARD</a></button>
             </td>
         </table>
     )
